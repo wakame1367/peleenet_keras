33abcf737c50b591870d0cb1c3d0e46c354716c6
wakamezake <hotwater1367@gmail.com>
2019-06-25 22:41:14 +0900

    origin pelee

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
-from keras.layers import Input, AveragePooling2D, Dense, Flatten, Dropout
-from keras.models import Model
+from tensorflow.python.keras.models import Model
+from tensorflow.python.keras.layers import (Input, GlobalAveragePooling2D, Dense,
+                                            Flatten, Dropout)
+
 from layers import stem_block, dense_block, transition_block, basic_conv_block
 
 
@@ -11,8 +13,7 @@ def pelee_net(input_shapes=(3, 224, 224), growth_rate=32, num_init_features=32,
     if block_configs is None:
         block_configs = [3, 4, 8, 6]
 
-    channel, height, width = input_shapes
-    x = Input(shape=(channel, height, width))
+    x = Input(shape=input_shapes)
 
     _stem_block = stem_block(x, num_init_features)
     total_filter = num_init_features
@@ -20,19 +21,17 @@ def pelee_net(input_shapes=(3, 224, 224), growth_rate=32, num_init_features=32,
     for idx, (block_config, bottleneck_width) in enumerate(zip(block_configs,
                                                                bottleneck_widths)):
         out = dense_block(_stem_block, num_layers=block_config,
-                          bn_size=bottleneck_width, growth_rate=growth_rate)
+                          bottleneck_width=bottleneck_width,
+                          growth_rate=growth_rate)
         total_filter += growth_rate * block_config
-        out = basic_conv_block(out, total_filter, kernel_size=1, strides=1,
-                               padding="valid")
-
         if idx == len(block_configs) - 1:
-            out = AveragePooling2D(pool_size=2, strides=2)(out)
-        # out = transition_block(out, total_filter, with_polling=with_pooling)
+            with_pooling = False
+        else:
+            with_pooling = True
+        out = transition_block(out, total_filter, with_polling=with_pooling)
         _stem_block = out
 
-    # out = AveragePooling2D(pool_size=(7, 7))(_stem_block)
-    out = Flatten()(_stem_block)
-
+    out = GlobalAveragePooling2D()(_stem_block)
     out = Dropout(rate=drop_rate)(out)
     outputs = Dense(num_classes, activation="softmax")(out)
     model = Model(inputs=x, outputs=outputs)
